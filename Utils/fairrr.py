9005c17b92a1955ddb546c63d1629ee9ded7803d
Khang <kt36@njit.edu>
2023-03-14 20:07:46 -0400

    init

--- Utils/fairrr.py
+++ Utils/fairrr.py
@@ -56,7 +56,7 @@ def fairRR(args, arr, y, z):
     eps = softmax(mi_label - args.alpha*mi_protect)*args.tar_eps
     p = sigmoid(eps)
     p = np.repeat(a=p, repeats=feat.shape[0], axis=0)
-    print(p.shape, feat.shape)
+    print("Shape of matrix:", p.shape, feat.shape)
     return
     p_temp = np.random.rand(p.shape[0], p.shape[1])
     perturb = (p_temp > p).astype(int)
